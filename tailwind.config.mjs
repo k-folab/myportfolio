4c9a951bbf74c108759e8f2e668d5a28228692f5
k-folab <emmanuelkehinde235@gmail.com>
2025-01-25 16:31:42 +0100

    navs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,8 +5,17 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Roboto", "Arial", "sans-serif"], // Add Google Font
+        poppins: ["Poppins", "serif"],
+        raleway: ["Raleway", "serif"],
+        nunito: ["Nunito", "serif"],
+        overlock: ["Overlock", "serif"],
+        merriweather: ["Merriweather", "serif"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
